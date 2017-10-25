6ef1e3a2cec5df7ecd4d77c82b73148f62a4cc44
Webmaster444 <jlmobile710@gmail.com>
2017-10-25 13:06:54 -0600

    update funcionality

--- newapp/app/controllers/group_events_controller.rb
+++ newapp/app/controllers/group_events_controller.rb
@@ -15,6 +15,19 @@ class GroupEventsController < ApplicationController
     	@ges = GroupEvent.all
   	end
 
+  	def edit
+  		@ge = GroupEvent.find(params[:id])
+  	end
+  	def update
+		@ge = GroupEvent.find(params[:id])
+ 
+	  	if @ge.update(groupevent_params)
+	    	redirect_to @ge
+	  	else
+	    	render 'edit'
+	  	end
+  	end
+
 	private
 	def groupevent_params
 	    params.require(:groupevent).permit(:name, :description, :startdate, :duration, :status)
