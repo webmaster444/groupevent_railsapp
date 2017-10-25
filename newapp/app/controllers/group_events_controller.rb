649a5ca65a57a40b988eb635011e6ccc7b2144bf
Webmaster444 <jlmobile710@gmail.com>
2017-10-25 14:30:27 -0600

    draft functionality added

--- newapp/app/controllers/group_events_controller.rb
+++ newapp/app/controllers/group_events_controller.rb
@@ -6,7 +6,7 @@ class GroupEventsController < ApplicationController
 	  @ge = GroupEvent.new(groupevent_params)
  
 	  @ge.save
-	  redirect_to  @ge
+	  redirect_to @ge
 	end
   	def show
     	@ge = GroupEvent.find(params[:id])
@@ -21,18 +21,36 @@ class GroupEventsController < ApplicationController
   	def update
 		@ge = GroupEvent.find(params[:id])
  
-	  	if @ge.update(groupeventupdate_params)
+	  	if @ge.update(groupevent_params)
 	    	redirect_to @ge
 	  	else
 	    	render 'edit'
 	  	end
   	end
 
-	private
-	def groupevent_params
-	    params.require(:groupevent).permit(:name, :description, :startdate, :duration, :status)
+  	def publish
+		@ge = GroupEvent.find(params[:id])
+ 		 		
+	  	if @ge.update('status': 'Published')
+	    	redirect_to @ge
+	  	else
+	    	render 'edit'
+	  	end
 	end
-	def groupeventupdate_params
+  	
+  	def draft
+		@ge = GroupEvent.find(params[:id])
+ 		 		
+	  	if @ge.update('status': 'Draft')
+	    	redirect_to @ge
+	  	else
+	    	render 'edit'
+	  	end
+	end
+	
+	private
+	def groupevent_params		
+		abort params.inspect
 	    params.require(:group_event).permit(:name, :description, :startdate, :duration, :status)
 	end
 end
--- newapp/config/routes.rb
+++ newapp/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'group_events#index'
-  resources :group_events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :group_events  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  	
+	get "/group_events/:id/publish", to:"group_events#publish" , as: "publishevent"			
+	get "/group_events/:id/draft", to:"group_events#draft" , as: "makedraftevent"			
 end
