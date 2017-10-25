7524e32173642dacbe5889fdf5c4fc219e5491d6
Webmaster444 <jlmobile710@gmail.com>
2017-10-25 12:25:26 -0600

    new project added

--- myapp/app/models/group_event.rb
+++
@@ -1,2 +0,0 @@
-class GroupEvent < ApplicationRecord
-end
--- myapp/db/migrate/20171025150748_add_duration_to_group_events.rb
+++
@@ -1,5 +0,0 @@
-class AddDurationToGroupEvents < ActiveRecord::Migration[5.1]
-  def change
-    add_column :group_events, :duration, :number
-  end
-end
